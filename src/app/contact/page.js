eb209177b0ff5cbdba508699a48463d3c10466f6
mgispert <mcg-511@hotmail.com>
2024-07-02 12:05:34 +0200

    add(contact): add email and street team form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,7 @@
-import { Box, Heading, Link } from "@chakra-ui/react";
+import { Box, Heading, Link, Text } from "@chakra-ui/react";
 import { FaInstagram } from "react-icons/fa";
+import { RiTeamLine } from "react-icons/ri";
+import { MdOutlineMailOutline } from "react-icons/md";
 import Navbar from "@/components/Navbar";
 
 export default function ContactPage() {
@@ -18,15 +20,27 @@ export default function ContactPage() {
         <Heading as="h1" size="xl" mb={4}>
           Contact Me!
         </Heading>
-        <Box display="flex" alignItems="center">
-          <FaInstagram size="24" />
-          <Link
-            href="https://www.instagram.com/marianne.the.author"
-            ml={2}
-            isExternal
-          >
-            @marianne.the.author
-          </Link>
+        <Box>
+          <Box display="flex" alignItems="center">
+            <FaInstagram size="24" />
+            <Link
+              href="https://www.instagram.com/marianne.the.author"
+              ml={2}
+              isExternal
+            >
+              @marianne.the.author
+            </Link>
+          </Box>
+          <Box display="flex" alignItems="center">
+            <RiTeamLine size="24" />
+            <Link href="https://forms.gle/QQQATFqaFiqNX4z4A" ml={2} isExternal>
+              Become a part of my street team!
+            </Link>
+          </Box>
+          <Box display="flex" alignItems="center">
+            <MdOutlineMailOutline size="24" />{" "}
+            <Text ml={2}>mariannegwrites@gmail.com</Text>
+          </Box>
         </Box>
       </Box>
     </>
--- src/data/books.js
+++ src/data/books.js
@@ -15,144 +15,145 @@ const books = [
       {
         id: "1",
         front: "/images/1.png",
-        back: "/images/16-16.png",
-      },
-      {
-        id: "2",
-        front: "/images/7.png",
-        back: "/images/6-6.png",
-      },
-      {
-        id: "3",
-        front: "/images/28.png",
-        back: "/images/28-28.png",
-      },
-      {
-        id: "4",
-        front: "/images/27.png",
-        back: "/images/26-26.png",
-      },
-      {
-        front: "/images/10.png",
-        back: "/images/9-9.png",
-      },
-      {
-        id: "6",
-        front: "/images/21.png",
-        back: "/images/21-21.png",
-      },
-      {
-        id: "7",
-        front: "/images/6.png",
-        back: "/images/5-5.png",
-      },
-      {
-        id: "8",
-        front: "/images/22.png",
-        back: "/images/22-22.png",
-      },
-      {
-        id: "9",
-        front: "/images/13.png",
-        back: "/images/11-11.png",
-      },
-      {
-        id: "10",
-        front: "/images/5.png",
-        back: "/images/3-3.png",
-      },
-      {
-        id: "11",
-        front: "/images/9.png",
-        back: "/images/7-7.png",
-      },
-      {
-        id: "12",
-        front: "/images/8.png",
-        back: "/images/8-8.png",
-      },
-      {
-        id: "13",
-        front: "/images/2.png",
         back: "/images/1-1.png",
       },
       {
-        id: "14",
-        front: "/images/24.png",
-        back: "/images/25-25.png",
-      },
-      {
-        id: "15",
-        front: "/images/18.png",
-        back: "/images/18-18.png",
-      },
-      {
-        id: "16",
-        front: "/images/11.png",
-        back: "/images/10-10.png",
-      },
-      {
-        id: "17",
-        front: "/images/26.png",
-        back: "/images/27-27.png",
-      },
-      {
-        id: "18",
-        front: "/images/17.png",
-        back: "/images/15-15.png",
-      },
-      {
-        id: "19",
-        front: "/images/25.png",
-        back: "/images/24-24.png",
-      },
-      {
-        id: "20",
-        front: "/images/15.png",
-        back: "/images/14-14.png",
-      },
-      {
-        id: "21",
-        front: "/images/23.png",
-        back: "/images/23-23.png",
-      },
-      {
-        id: "22",
-        front: "/images/3.png",
+        id: "2",
+        front: "/images/2.png",
         back: "/images/2-2.png",
       },
       {
-        id: "23",
-        front: "/images/14.png",
-        back: "/images/13-13.png",
+        id: "3",
+        front: "/images/3.png",
+        back: "/images/3-3.png",
       },
       {
-        id: "24",
-        front: "/images/20.png",
-        back: "/images/20-20.png",
-      },
-      {
-        id: "25",
-        front: "/images/19.png",
-        back: "/images/19-19.png",
-      },
-      {
-        id: "26",
+        id: "4",
         front: "/images/4.png",
         back: "/images/4-4.png",
       },
       {
-        id: "27",
+        id: "5",
+        front: "/images/5.png",
+        back: "/images/5-5.png",
+      },
+      {
+        id: "6",
+        front: "/images/6.png",
+        back: "/images/6-6.png",
+      },
+      {
+        id: "7",
+        front: "/images/7.png",
+        back: "/images/7-7.png",
+      },
+      {
+        id: "8",
+        front: "/images/8.png",
+        back: "/images/8-8.png",
+      },
+      {
+        id: "9",
+        front: "/images/9.png",
+        back: "/images/9-9.png",
+      },
+      {
+        id: "10",
+        front: "/images/10.png",
+        back: "/images/10-10.png",
+      },
+      {
+        id: "11",
+        front: "/images/11.png",
+        back: "/images/11-11.png",
+      },
+      {
+        id: "12",
         front: "/images/12.png",
         back: "/images/12-12.png",
       },
       {
-        id: "28",
+        id: "13",
+        front: "/images/13.png",
+        back: "/images/13-13.png",
+      },
+      {
+        id: "14",
+        front: "/images/14.png",
+        back: "/images/14-14.png",
+      },
+      {
+        id: "15",
+        front: "/images/15.png",
+        back: "/images/15-15.png",
+      },
+      {
+        id: "16",
         front: "/images/16.png",
+        back: "/images/16-16.png",
+      },
+      {
+        id: "17",
+        front: "/images/17.png",
         back: "/images/17-17.png",
       },
       {
+        id: "18",
+        front: "/images/18.png",
+        back: "/images/18-18.png",
+      },
+      {
         id: "19",
+        front: "/images/19.png",
+        back: "/images/19-19.png",
+      },
+      {
+        id: "20",
+        front: "/images/20.png",
+        back: "/images/20-20.png",
+      },
+      {
+        id: "21",
+        front: "/images/21.png",
+        back: "/images/21-21.png",
+      },
+      {
+        id: "22",
+        front: "/images/22.png",
+        back: "/images/22-22.png",
+      },
+      {
+        id: "23",
+        front: "/images/23.png",
+        back: "/images/23-23.png",
+      },
+      {
+        id: "24",
+        front: "/images/24.png",
+        back: "/images/24-24.png",
+      },
+      {
+        id: "25",
+        front: "/images/25.png",
+        back: "/images/25-25.png",
+      },
+      {
+        id: "26",
+        front: "/images/26.png",
+        back: "/images/26-26.png",
+      },
+      {
+        id: "27",
+        front: "/images/27.png",
+        back: "/images/27-27.png",
+      },
+      {
+        id: "28",
+        front: "/images/28.png",
+        back: "/images/28-28.png",
+      },
+      {
+        id: "29",
         front: "/images/29.png",
         back: "/images/29-29.png",
       },
